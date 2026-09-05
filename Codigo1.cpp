b68d2ed7f750e4ea84239a6cd1558f055ca0545a
RobinMA-Des <robinmoyaaros@gmail.com>
2026-09-05 15:25:04 -0400

    commit

---
+++ Codigo1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std
+
+string PersonajeTieneEspada (Int espada)
+{
+    string respuesta = "";
+    if (Espada <= 0)
+    {
+        Respuesta = "No";
+    }
+    else
+    {
+        Respuesta = "Si";
+    }
+    return respuesta;
+}
+
+string mostrarFicha (string nombre, int vida, int coins, int espada, float speed bool Llave)
+{
+    cout << "" << endl;
+    cout << "Ficha tecnica << endl;
+    cout << "Nombre: " << nombre << endl";
+    cout << "Vida: " << vida << endl;
+    cout << "Nivel: " << nivel << endl;
+    cout << "Monedas: " << coins << endl;
+    cout << "Velocidad: " << speed << endl;
+    cout << "Tiene espada?" << PersonajeTieneEspada(Espada) << endl;
+    cout << "Tiene llave?: " << Llave << endl;
+    cout << "" << endl;
+}
+
+
+int main()
+}
+    }
+    }
+}
