2c7bd5636a9c83f170c04c19417023132674af01
RobinMA-Des <robinmoyaaros@gmail.com>
2026-09-05 15:48:23 -0400

    commit

--- Codigo1.cpp
+++ Codigo1.cpp
@@ -29,3 +29,27 @@ string mostrarFicha (string nombre, int vida, int coins, int espada, float speed
     cout << "" << endl;
 }
 
+string accesoAreaBosque (int nivel, bool llave)
+{
+    string respuesta = "";
+    if (nivel >= 5 && llave == true)
+    {
+        respuesta = "Acceso al bosque desbloqueado";
+    }
+    else
+    {
+        respuesta = "Acceso al bosque denegado";
+    }
+    return respuesta;
+}
+    Int main ()
+    {
+    string nombrePersonaje = "Aragorn";
+    int vida = 100;
+    int coins = 0;
+    int espada = 10;
+    float speed = 10.0;
+    bool llave = true;
+    int nivel = 5;
+
+    mostrarFicha(nombrePersonaje, vida, nivel, coins, espada, speed, llave);
